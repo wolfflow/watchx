6bfef7d3fdc48deeea0ac32e737dc5793b123d31
Daniel K <hldr@ya.ru>
2016-04-01 11:05:56 +0000

    handler code de-duped

--- actions.js
+++ actions.js
@@ -3,17 +3,17 @@ var EventEmitter = require('events').EventEmitter,
     spawn        = require('child_process').spawn,
     CONFIG       = {}
 
-actions.on('SINGLE_CHANGE', function(path) {
-  var parameters = [CONFIG['ACTIONS']['SINGLE_CHANGE'], path]
-  var command = spawn('bash', parameters)
-  command.stdout.pipe(process.stdout)
-})
+var makeHandler = function(type){
+  return function (path){
+    var parameters = [CONFIG['ACTIONS'][type], path]
+    var command = spawn('bash', parameters)
+    command.stdout.pipe(process.stdout)  
+  }  
+}
 
-actions.on('MULTIPLE_CHANGE', function(paths) {
-  var parameters = [CONFIG['ACTIONS']['MULTIPLE_CHANGE']].concat(paths)
-  var command = spawn('bash', parameters)
-  command.stdout.pipe(process.stdout)
-})
+actions.on('SINGLE_CHANGE', makeHandler('SINGLE_CHANGE'))
+
+actions.on('MULTIPLE_CHANGE', makeHandler('MULTIPLE_CHANGE'))
 
 module.exports = function(config) {
   CONFIG = config
